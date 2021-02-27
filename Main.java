85765aab4d83a3a917d83cf26ca8c46a174a3a41
RNorris6 <ryan.norris@patelhs.org>
2021-02-27 04:17:25 +0000

    final product starting to put together comments.

--- Main.java
+++ Main.java
@@ -7,23 +7,21 @@ public class Main {
 
     Scanner scan = new Scanner(System.in);// inputing scanner system.
 
-    System.out.println("The clock is going to strike 12 in a couple seconds here, is everyone excited for the new year, this next year is going to be the year...");
-    int year = scan.next();
-      System.out.println("I'm your host Brad peterson bringing you the countdown to" + year);
+    System.out.println("The clock is going to strike 12 in a couple seconds here, is everyone excited for the new year, what are your resolutions?");
+    String resolutions = scan.next();
+      System.out.println("I'm glad to see that your resolution is" + resolutions + ", now back to the countdown");
       System.out.println("Get excited for the new year in 11...");
       newYearsTimer(10);
   }
-
+//start of recursion statement stating the countdown and future resolutions.
   public static void newYearsTimer(int timerValue)
   {
-    if (timerValue == 1)
+    if (timerValue == 0)// base statement to stop the cycle.
       {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("Happy new year everyone");
+        System.out.println("Lets wish for our resolutions to become true and make this year better than the last");
       }
-    else{
+    else{//recursion statements to click down from 10-1
       System.out.println(timerValue);
       timerValue--;
       newYearsTimer(timerValue);
